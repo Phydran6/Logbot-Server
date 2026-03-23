7f907ec694d7ffa3730b0a128ecbb3f09d5c9786
Phydran6 <fischerit@phytech.de>
2026-03-23 19:34:11 +0100

    Update author name in install-windows.bat

--- agents/install-windows.bat
+++ agents/install-windows.bat
@@ -1,7 +1,6 @@
 @echo off
 :: ==============================================================================
-:: Name:        Philipp Fischer
-:: Kontakt:     p.fischer@itconex.de
+:: # Name:        Phydran6
 :: Version:     2026.01.30.13.30.00
 :: Beschreibung: LogBot Agent v2026.01.30.13.30.00 - Windows Installer Wrapper
 :: ==============================================================================
