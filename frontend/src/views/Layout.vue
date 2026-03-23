d3e34745e05d91f2b7aba449cd49feaf94078ff1
Phydran6 <fischerit@phytech.de>
2026-03-23 19:29:16 +0100

    Remove personal information from Layout.vue
    Removed personal contact information from Layout.vue.

--- frontend/src/views/Layout.vue
+++ frontend/src/views/Layout.vue
@@ -1,6 +1,5 @@
 ﻿<!-- ==============================================================================
-     Name:        Philipp Fischer
-     Kontakt:     p.fischer@itconex.de
+     # Name:        Phydran6
      Version:     2026.03.19.13.26.52
      Beschreibung: LogBot - Hauptlayout mit Sidebar/Navigation
      ============================================================================== -->
